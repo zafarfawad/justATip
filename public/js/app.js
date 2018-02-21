a3d07a5a2b5439e45e3458076dc048f1035939e0
Fawad Zafar <fawad.zafar@robotsandpencils.com>
2018-02-21 15:29:29 -0500

    Fixed DB issues,date isses, moment library

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,31 @@
-var salary = 5.03;
-var salaryTotalDaily;
-var totalDaily;
-var perHourDaily;
+// var salary = 5.03;
+// var salaryTotalDaily;
+// var totalDaily;
+// var perHourDaily;
 
-$("#submit_tip").on("click", function () {
-    var tipAmountDaily = $("#input_tip_amount").val();
-    var hoursWorkedDaily = $("#input_hours_worked").val();
-    var minutesWorkedDaily = $("#input_minutes_worked").val()
-    var notes = $("#input_notes").val();
-    dailyWage();
-    weeklyWage();
+// $("#submit_tip").on("click", function () {
+//     var tipAmountDaily = $("#input_tip_amount").val();
+//     var hoursWorkedDaily = $("#input_hours_worked").val();
+//     var minutesWorkedDaily = $("#input_minutes_worked").val()
+//     var notes = $("#input_notes").val();
+//     dailyWage();
+//     weeklyWage();
 
-    function dailyWage() {
-        totalHoursWorkedDaily = (parseFloat(hoursWorkedDaily) + parseFloat(minutesWorkedDaily));
-        totalSalaryDaily = (salary * hoursWorkedDaily).toFixed(2);
-        totalWageDaily = (parseFloat(tipAmountDaily) + parseFloat(totalSalaryDaily)).toFixed(2)
-        totalHourlyDaily = (totalWageDaily / parseFloat(totalHoursWorkedDaily)).toFixed(2);
-        console.log(totalHoursWorkedDaily)
+//     function dailyWage() {
+//         totalHoursWorkedDaily = (parseFloat(hoursWorkedDaily) + parseFloat(minutesWorkedDaily));
+//         totalSalaryDaily = (salary * hoursWorkedDaily).toFixed(2);
+//         totalWageDaily = (parseFloat(tipAmountDaily) + parseFloat(totalSalaryDaily)).toFixed(2)
+//         totalHourlyDaily = (totalWageDaily / parseFloat(totalHoursWorkedDaily)).toFixed(2);
+//         console.log(totalHoursWorkedDaily)
 
-        $("#total_weekly_amount").text(totalWageDaily);
-        $("#total_weekly_hours").text(moment.duration(totalHoursWorkedDaily, "minutes").format());
-        $("#total_weekly_hourly").text(totalHourlyDaily);
-        $("#total_weekly_tips").text(tipAmountDaily);
+//         $("#total_weekly_amount").text(totalWageDaily);
+//         $("#total_weekly_hours").text(moment.duration(totalHoursWorkedDaily, "minutes").format());
+//         $("#total_weekly_hourly").text(totalHourlyDaily);
+//         $("#total_weekly_tips").text(tipAmountDaily);
 
-    }
+//     }
 
-    function weeklyWage() {
-        console.log("This is working!")
-    }
-});
+//     function weeklyWage() {
+//         console.log("This is working!")
+//     }
+// });
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -21,22 +21,55 @@ module.exports = function(app) {
     });
   });
     // POST route for saving a new day
+    
+    
     app.post("/api/day", function(req, res) {
-        // create takes an argument of an object describing the item we want to
-        // insert into our table. In this case we just we pass in an object with a text
-        // and complete property
+      var salary = 5.03;
+
+      function dailyWage() {
+        totalHoursWorkedDaily = (parseFloat(req.body.hoursWorkedDaily) + parseFloat(req.body.minutesWorkedDaily));
+        totalSalaryDaily = (salary * req.body.hoursWorkedDaily).toFixed(2);
+        totalWageDaily = (parseFloat(req.body.tipAmountDaily) + parseFloat(totalSalaryDaily)).toFixed(2)
+        totalHourlyDaily = (totalWageDaily / parseFloat(totalHoursWorkedDaily)).toFixed(2);
+        console.log(totalHoursWorkedDaily);
+
+        return {
+          totalHoursWorkedDaily: totalHoursWorkedDaily,
+          totalSalaryDaily: totalSalaryDaily,
+          totalWageDaily: totalWageDaily,
+          totalHourlyDaily: totalHourlyDaily
+        } 
+      } 
+
+      dailyWage();
+
+      console.log('fawad',totalWageDaily);
+
         db.day.create({
-          input_hours_worked: req.body.selection1,
-          input_tip_amount: req.body.selection2,
-          input_notes: req.body.selection3,
+          input_hours_worked: req.body.hoursWorkedDaily,
+          input_minutes_worked: req.body.minutesWorkedDaily,
+          input_totalhours_worked: totalHoursWorkedDaily,
+          input_tip_amount: req.body.tipAmountDaily,
+          input_daily_wage: totalWageDaily,
+          input_hourly_wage: totalHourlyDaily,
+          input_notes: req.body.notes,
+
         }).then(function(dbday) {
           // We have access to the new day as an argument inside of the callback function
           res.json(dbday);
+          console.log('backend', dbday.id)
+
         });
-        console.log('fawad', req.body)
     });
         // DELETE route for deleting day. We can get the id of the day to be deleted from
   // req.params.id
+  
+  
+  
+  
+  
+  
+  
   app.delete("/api/day/:id", function(req, res) {
     // We just have to specify which day we want to destroy with "where"
     db.day.destroy({
